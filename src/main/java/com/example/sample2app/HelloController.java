c67f79e5eb61e99d20859d3bf5742a423810addf
sea-yassan33 <a.yasuda_tr51@me.com>
2023-05-27 13:08:31 +0900

    thymeleaf

--- src/main/java/com/example/sample2app/HelloController.java
+++ src/main/java/com/example/sample2app/HelloController.java
@@ -1,13 +1,19 @@
 package com.example.sample2app;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
   @RequestMapping("/")
-  public String index(){
-    return "index";
+  public ModelAndView index(ModelAndView mav){
+    mav.setViewName("index");
+    mav.addObject("title", "Hello page");
+    mav.addObject("msg","this is JPA sample data");
+    return mav;
   }
 }
