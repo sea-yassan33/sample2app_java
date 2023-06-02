3eb07158e1dde2cba828ebddf252342bd78ec280
sea-yassan33 <a.yasuda_tr51@me.com>
2023-06-02 14:27:46 +0900

    CRUD5

--- src/main/java/com/example/sample2app/repositories/PersonRepository.java
+++ src/main/java/com/example/sample2app/repositories/PersonRepository.java
@@ -1,5 +1,6 @@
 package com.example.sample2app.repositories;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +9,11 @@ import org.springframework.stereotype.Repository;
 import com.example.sample2app.Person;
 
 @Repository
-public interface PersonRepository extends JpaRepository<Person, Long>{
+public interface PersonRepository
+        extends JpaRepository<Person, Long>{
   public Optional<Person> findById(Long name);
+  public List<Person> findByNameLike(String name);
+  public List<Person> findByIdIsNotNullOrderByIdDesc();
+  public List<Person> findByAgeGreaterThan(Integer age);
+  public List<Person> findByAgeBetween(Integer age1, Integer age2);
 }
