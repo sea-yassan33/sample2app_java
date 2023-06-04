302a6ddb020683d1442ad10ac5607d881b4a0355
sea-yassan33 <a.yasuda_tr51@me.com>
2023-06-04 14:40:06 +0900

    EntityManager4

--- src/main/java/com/example/sample2app/PersonDAO.java
+++ src/main/java/com/example/sample2app/PersonDAO.java
@@ -4,4 +4,6 @@ import java.io.Serializable;
 import java.util.List;
 public interface PersonDAO <T> extends Serializable {
   public List<T> getAll();
+  public T findById(long id);
+  public List<T> findByName(String name);
 }
